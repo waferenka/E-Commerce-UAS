06297e64603cf352bbb4aa85bee1e96ce765ad06
Rangga Ayi Pratama <dddoos123456789@gmail.com>
2024-12-11 19:27:37 +0700

    Memperbaiki struktur file js dan css

--- login/login_form.php
+++ login/login_form.php
@@ -1,3 +1,4 @@
+<!-- Seesion Start -->
 <?php
 session_start();
 if (isset($_SESSION['username'])) {
@@ -13,6 +14,7 @@ if (isset($_SESSION['username'])) {
     exit;
 }
 ?>
+<!-- Session End -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,8 +22,11 @@ if (isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- My Style -->
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
