6eae1f71ba7c66b9caf60b93fe54801c85a10f0b
Rangga Ayi Pratama <dddoos123456789@gmail.com>
2024-12-14 03:01:28 +0700

    Penambahan metadata

---
+++ metadata.php
@@ -0,0 +1,26 @@
+<!-- Metadata start -->
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="MusicNest." />
+    <meta
+      property="og:description"
+      content="MusicNest adalah platform streaming musik berbasis web yang dibangun dengan PHP dan Bootstrap. Aplikasi ini memungkinkan pengguna untuk menelusuri musik berdasarkan artis dan album, serta memutar lagu langsung dari browser."
+    />
+    <meta
+      property="og:image"
+      content="https://nhs-lodge-song-pace.trycloudflare.com/Rangga_Ayi_Pratama_062330701526/Music_Stream/img/thumbnail-og.avif"
+    />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="1200" />
+    <meta property="og:url" content="https://nhs-lodge-song-pace.trycloudflare.com/Rangga_Ayi_Pratama_062330701526/Music_Stream/" />
+    <link rel="icon" href="img/thumbnail-og.avif" type="image/x-icon" /> 
+    <!-- Metadata end -->
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- My style -->
+    <link rel="stylesheet" href="style/style.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Qwitcher+Grypen:wght@400;700&display=swap" rel="stylesheet">
