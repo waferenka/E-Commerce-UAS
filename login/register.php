5c8b876a1856b9f69c6dea9572c7683a3229d1f3
Rangga Ayi Pratama <dddoos123456789@gmail.com>
2024-12-13 17:44:46 +0700

    register

--- login/register.php
+++ login/register.php
@@ -31,30 +31,98 @@ if (isset($_POST['register'])) {
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- My Style -->
     <link rel="stylesheet" href="../css/bootstrap_style.css">
+
+    <style>
+    html,
+    body {
+        overflow-y: hidden;
+        width: 100%;
+        height: 100%;
+    }
+
+    .container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+    }
+
+    form {
+        width: 80%;
+        height: 50%;
+    }
+
+    .table_custom {
+        /* --bs-table-bg: transparent !important; */
+    }
+
+    th {
+        text-align: center;
+        vertical-align: middle;
+    }
+
+    th .form-label {
+        display: block;
+        text-align: center;
+    }
+    </style>
 </head>
 
 <body class="bg-light">
-    <div class="container mt-5">
-        <h2 class="text-center">Register</h2>
+    <!-- Navbar Start -->
+    <nav class="navbar bg-body-secondary">
+        <div class="container-fluid ms-3 me-3">
+            <a class="navbar-brand me-4 logo" href="#">
+                <img class="me-3" src="../favicon.ico" alt="">Alzi Petshop</a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+    <div class="container">
         <?php if (isset($error)): ?>
         <div class="alert alert-danger text-center"><?= $error; ?></div>
         <?php endif; ?>
         <form action="#" method="post">
-            <div class="form-group mb-3">
-                <label for="nama" class="form-label">Nama</label>
-                <input type="text" name="nama" id="nama" class="form-control" required>
-            </div>
-            <div class="form-group mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" id="email" class="form-control" required>
-            </div>
-            <div class="form-group mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" id="password" class="form-control" required>
-            </div>
-            <button type="submit" name="register" class="btn btn-warning w-100 mt-2">Register</button>
+            <table class="table table-bordered table_custom">
+                <tbody>
+                    <tr>
+                        <th colspan="2" scope="row">
+                            <h2 class="text-center">Register</h2>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="nama" class="form-label">Nama</label></th>
+                        <td><input type="text" name="nama" id="nama" class="form-control" required></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="email" class="form-label">Email</label></th>
+                        <td><input type="email" name="email" id="email" class="form-control" required></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="password" class="form-label">Password</label></th>
+                        <td><input type="password" name="password" id="password" class="form-control" required></td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row"><a href="../index.php" type="reset" name="cancel"
+                                class="btn btn-danger w-100 mt-2">Cancel</a></th>
+                        <td><button type="submit" name="register" class="btn btn-warning w-100 mt-2">Register</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </form>
     </div>
+
+    <footer class="text-center">
+        <p>Create by Alzi Petshop | &copy 2024</p>
+    </footer>
 </body>
 
 </html>
