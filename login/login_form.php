a05b0a65a4b081a2ec1260bdb795dfd5ece333f9
Adit <waferenak512@gmail.com>
2025-01-02 14:52:46 +0700

    login, admin

--- login/login_form.php
+++ login/login_form.php
@@ -3,7 +3,7 @@
 session_start();
 if (isset($_SESSION['email'])) {
     if ($_SESSION['level'] == "admin") {
-        header("Location: ../index_a.php");
+        header("Location: ../admin/admin.php");
     } elseif ($_SESSION['level'] == "penjual") {
         header("Location: ../index_p.php");
     } elseif ($_SESSION['level'] == "pembeli") {
