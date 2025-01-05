160e8f3330dd185915690b64138f1e31bb63eec5
Rangga Ayi Pratama <103559445+RLA07@users.noreply.github.com>
2025-01-05 18:40:41 +0700

    Update midtrans_config.php

--- midtrans_config.php
+++ midtrans_config.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/midtrans-php/Midtrans.php';
 
 // Set your Merchant Server Key
-\Midtrans\Config::$serverKey = 'SB-Mid-server-hI81QFjkGWJvQmZhcWea5GUU';
+\Midtrans\Config::$serverKey = 'ISI SERVER KEY';
 // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
 \Midtrans\Config::$isProduction = false;
 // Set sanitization on (default)
