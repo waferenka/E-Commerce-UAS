87678315bc34e0c77635f69ed41f8592e24d4943
Rangga Ayi Pratama <dddoos123456789@gmail.com>
2025-01-05 11:40:36 +0700

    midtrans

---
+++ midtrans_config.php
@@ -0,0 +1,39 @@
+<?php
+/*Install Midtrans PHP Library (https://github.com/Midtrans/midtrans-php)
+composer require midtrans/midtrans-php
+
+Alternatively, if you are not using **Composer**, you can download midtrans-php library
+(https://github.com/Midtrans/midtrans-php/archive/master.zip), and then require
+the file manually.
+
+require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
+
+
+require_once dirname(__FILE__) . './midtrans-php/Midtrans.php';
+
+//SAMPLE REQUEST START HERE
+
+// Set your Merchant Server Key
+\Midtrans\Config::$serverKey = 'Mid-server-O-m7nSmg0fQut16CHF6MMNVl';
+// Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+\Midtrans\Config::$isProduction = false;
+// Set sanitization on (default)
+\Midtrans\Config::$isSanitized = true;
+// Set 3DS transaction for credit card to true
+\Midtrans\Config::$is3ds = true;
+
+$params = array(
+'transaction_details' => array(
+'order_id' => rand(),
+'gross_amount' => 10000,
+),
+'customer_details' => array(
+'first_name' => 'budi',
+'last_name' => 'pratama',
+'email' => 'budi.pra@example.com',
+'phone' => '08111222333',
+),
+);
+
+$snapToken = \Midtrans\Snap::getSnapToken($params);
+?>
